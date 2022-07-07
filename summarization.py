618e9600ef6949312aeda0ede5a6eb5e39975d61
KDercksen <mail@koendercksen.com>
2022-07-07 14:28:33 +0200

    add transformer summarizer code

--- summarization.py
+++ summarization.py
@@ -1,7 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 from utils import preprocess
+from hydra import initialize, compose
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from utils import LazyValueDict
+import torch as t
+
+
+class BaseSummarizer:
+    def summarize(self, text: str):
+        raise NotImplementedError("subclass should implement this function")
+
+
+class TransformerSummarizer(BaseSummarizer):
+    def __init__(self):
+        with initialize("conf", version_base="1.1"):
+            cfg = compose("summarize/transformer.yaml")["summarize"]
+        self.device = "cuda" if t.cuda.is_available() else "cpu"
+        self.tokenizer = AutoTokenizer.from_pretrained(cfg["tokenizer"])
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(cfg["model"]).to(self.device)
+        self.max_len = cfg["max_generation_length"]
+
+    def summarize(self, text: str):
+        tokenized = self.tokenizer(text, return_tensors="pt").to(self.device)
+        outputs = (
+            self.model.generate(**tokenized, max_length=self.max_len).detach().cpu()
+        )
+        return self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
+
+summarizers = LazyValueDict(
+    {
+        "transformer": TransformerSummarizer,
+    }
+)
 
 
 def get_summary(text: str, model_name: str):
-    return {"summary": preprocess(text)["text"]}
+    preprocessed = preprocess(text)
+    summary = summarizers[model_name].summarize(preprocessed["text"])
+    return {"summary": summary}
